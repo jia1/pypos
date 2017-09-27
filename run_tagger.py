ca1d5d8663edebfab249ecc0f0afaf5b12f40d1a
Jia Yee <jiayeerawr@gmail.com>
2017-09-27 23:09:46 +0800

    Fix run_tagger compile errors
    1. Cast parsed num_tags to int
    2. Move viterbi function definition above function call
    3. Swap viterbi and backpointer matrix dimensions
    4. Add key checks for emission dictionary
    5. Add len() to T because T is a list of tokens and not a number
    6. Translate viterbi output to POS tags and write result to file
    
    Signed-off-by: Jia Yee <jiayeerawr@gmail.com>

--- run_tagger.py
+++ run_tagger.py
@@ -29,17 +29,12 @@ with open('pos.key', 'r') as k:
     num_tags += 1
 
 with open(model_file, 'r') as m:
-    num_tags = next(m)
+    num_tags = int(next(m))
     for line in m:
         mat_transitions.append(list(map(lambda p: Decimal(p), line.strip().split(' '))))
 
 with open(emit_file, 'r') as e:
     map_emissions = json.load(e)
-
-with open(test_file, 'r') as f, open(result_file, 'w') as g:
-    for line in f:
-        tokens = line.strip().split(' ')
-        viterbi(tokens)
 
 a, b = mat_transitions, map_emissions
 
@@ -47,17 +42,25 @@ def viterbi(T):
     global a # N*N: P(curr_tag|prev_tag) where N = num_tags
     global b # map: P(curr_tag|curr_token)
 
-    mat_viterbi = [[0 for col in range(num_tags)] for row in range(T)]
-    mat_backptr = [[-1 for col in range(num_tags)] for row in range(T)]
+    mat_viterbi = [[0 for col in range(len(T))] for row in range(num_tags)]
+    mat_backptr = [[-1 for col in range(len(T))] for row in range(num_tags)]
 
     for s in range(num_tags):
-        mat_viterbi[s][0] = a[0][s] * b[T[0]][pos_tags_list[s]]
+        token, tag = T[0], pos_tags_list[s]
+        if token not in b or tag not in b[token]:
+            mat_viterbi[s][0] = 0
+        else:
+            mat_viterbi[s][0] = a[0][s] * Decimal(b[token][tag])
     for t in range(1, len(T)):
         for s in range(num_tags):
             v_max_prob, v_max_s, b_max_prob, b_max_s = 0, 0, 0, 0
             for s_prime in range(num_tags):
                 b_curr_prob = mat_viterbi[s_prime][t-1] * a[s_prime][s]
-                v_curr_prob = b_curr_prob * b[T[t-1]][pos_tags_list[s_prime]]
+                token, tag = T[t-1], pos_tags_list[s_prime]
+                if token not in b or tag not in b[token]:
+                    v_curr_prob = 0
+                else:
+                    v_curr_prob = b_curr_prob * Decimal(b[token][tag])
                 if v_curr_prob > v_max_prob:
                     v_max_prob = v_curr_prob
                     v_max_s = s_prime
@@ -69,7 +72,11 @@ def viterbi(T):
 
     for s_prime in range(num_tags):
         b_curr_prob = mat_viterbi[s_prime][-1] * a[s_prime][s]
-        v_curr_prob = b_curr_prob * b[T[-1]][pos_tags_list[s_prime]]
+        token, tag = T[-1], pos_tags_list[s_prime]
+        if token not in b or tag not in b[token]:
+            v_curr_prob = 0
+        else:
+            v_curr_prob = b_curr_prob * Decimal(b[token][tag])
         if v_curr_prob > v_max_prob:
             v_max_prob = v_curr_prob
             v_max_s = s_prime
@@ -81,7 +88,18 @@ def viterbi(T):
 
     max_s = mat_backptr[-1][-1]
     fin_back_path = []
-    for t in range(T-1, 0, -1):
+    for t in range(len(T)-1, 0, -1):
         fin_back_path.append(max_s)
         max_s = mat_backptr[max_s][t-1]
+    fin_back_path.append(max_s)
     return fin_back_path[::-1]
+
+with open(test_file, 'r') as f, open(result_file, 'w') as g:
+    for line in f:
+        tokens = line.strip().split(' ')
+        tags = list(map(lambda index: pos_tags_list[index], viterbi(tokens)))
+
+        tagged_tokens = []
+        for i in range(len(tokens)):
+            tagged_tokens.append('{0}/{1}'.format(tokens[i], tags[i]))
+        g.write('{0}\n'.format(' '.join(tagged_tokens)))
