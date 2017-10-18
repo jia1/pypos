387d370a87d3f73497d551648d859255f174cd56
JIA YEE <jiayeerawr@gmail.com>
2017-10-19 02:27:05 +0800

    Move result files to results folder and ignore backup files
    Add files from sunfire
    
    Signed-off-by: JIA YEE <jiayeerawr@gmail.com>

--- validate.sh
+++ validate.sh
@@ -1,2 +1,2 @@
 export PATH=$PATH:/usr/local/Python-3.6.3/bin
-nohup time python3 cross_validate.py data/sents.train &> nohup-cross-validate.out &
+nohup time python3 cross_validate.py data/sents.train &> nohup-cross-validate-05-2.out &
